d880e6bb77ee0dd25a4c821948435a0263444621
张晓阳 <zhangxy@guoguokeji.com.cn>
2020-05-27 20:20:36 +0800

    1

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,101 +1,79 @@
-// pages/addFunction/addFunction.js
-
-const code = `// 云函数入口函数
-exports.main = (event, context) => {
-  console.log(event)
-  console.log(context)
-  return {
-    sum: event.a + event.b
-  }
-}`
-
 Page({
-   data: {
+  data: {
+    latitude: 39.90469,
+    longitude: 116.40717,
     markers: [{
-      iconPath: "../resources/others.png",
-      id: 0,
-      latitude: 23.099994,
-      longitude: 113.324520,
-      width: 50,
-      height: 50
-    }],
-    polyline: [{
-      points: [{
-        longitude: 113.3245211,
-        latitude: 23.10229
-      }, {
-        longitude: 113.324520,
-        latitude: 23.21229
-      }],
-      color:"#FF0000DD",
-      width: 2,
-      dottedLine: true
-    }],
-    controls: [{
       id: 1,
-      iconPath: '../resources/location.png',
-      position: {
-        left: 0,
-        top: 300 - 50,
-        width: 50,
-        height: 50
-      },
-      clickable: true
-    }]
+      latitude: 39.90469,
+      longitude: 116.40717,
+      title: "公司",
+      zIndex:1
+    },{
+      id: 2,
+      latitude: 38,
+      longitude: 115,
+      title: "家",
+      zIndex:2
+    }],
+    // covers: [{
+    //   latitude: 39,
+    //   longitude: 116,
+    //   iconPath: '../images/location.png'
+    // }, {
+    //   latitude: 39.90469,
+    //   longitude: 116.40717,
+    //   iconPath: '../images/location.png'
+    // }]
   },
-  regionchange(e) {
-    console.log(e.type)
+  onReady: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
   },
-  markertap(e) {
-    console.log(e.detail.markerId)
-  },
-  controltap(e) {
-    console.log(e.detail.controlId)
-  },
-
-  onLoad: function (options) {
-   /*  wx.chooseLocation({
-      complete: (res) => {
-        var latitude = res.latitude
-        var longitude = res.longitude
-      },
-    }) */
-  },
-
-  markertap: function() {
-    /* wx.getLocation({
-      type: 'gcj02 ',
-      success: (res) => {
-        var latitude = res.latitude // 纬度
-        var longitude = res.longitude // 经度
-      }
-    }) */
-  },
-
-  testFunction() {
-    wx.cloud.callFunction({
-      name: 'sum',
-      data: {
-        a: 1,
-        b: 2
-      },
-      success: res => {
-        wx.showToast({
-          title: '调用成功',
-        })
-        this.setData({
-          result: JSON.stringify(res.result)
-        })
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '调用失败',
-        })
-        console.error('[云函数] [sum] 调用失败：', err)
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function(res){
+        console.log(res.longitude)
+        console.log(res.latitude)
       }
     })
   },
-
+  getLocation:function(){
+    wx.getLocation({
+      type: "gcj02",
+      altitude: 'false',
+      isHighAccuracy: true,
+      success:function(res){
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+  },
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
+  },
+  translateMarker: function() {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude:23.10229,
+        longitude:113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  includePoints: function() {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude:23.10229,
+        longitude:113.3345211,
+      }, {
+        latitude:23.00229,
+        longitude:113.3345211,
+      }]
+    })
+  }
 })
-
